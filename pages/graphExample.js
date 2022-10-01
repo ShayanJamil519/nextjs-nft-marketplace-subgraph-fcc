bc95824ff1e0345740797e79b97d73f79c6b8cf5
Shayan Jamil <shayanjamil500@gmail.com>
2022-10-01 14:42:46 +0500

    Final commit of Nextjs NFT-Marketplace-SubGraph

---
+++ pages/graphExample.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_ACTIVE_ITEMS = gql`
+  {
+    activeItems(first: 5, where: { buyer: "0x00000000" }) {
+      id
+      buyer
+      seller
+      nftAddress
+      tokenId
+      price
+    }
+  }
+`;
+
+const graphExample = () => {
+  const { loading, error, data } = useQuery(GET_ACTIVE_ITEMS);
+  console.log(data);
+  return <div>hi</div>;
+};
+
+export default graphExample;
